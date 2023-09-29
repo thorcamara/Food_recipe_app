ea1bb78ad2956bb6769ba8ddd93e663f2650c052
thorcamara <thorcamaraalthoff@gmail.com>
2023-09-29 10:28:38 -0300

    Added functions

--- script.js
+++ script.js
@@ -23,3 +23,42 @@ mealList.addEventListener('click', async (e) => {
     }
   }
 });
+
+async function searchMealsByIngredient(ingredient) {
+  try {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+    const data = await response.json();
+    return data.meals;
+  } catch (error) {
+    // Show error in console
+    console.error('Error fetching data:', error);
+  }
+}
+
+async function getMealDetails(mealId) {
+  try {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
+    const data = await response.json();
+    return data.meals[0];
+  } catch (error) {
+    console.error('Error fetching meal details:', error);
+  }
+}
+
+function displayMeals(meals) {
+  mealList.innerHTML = '';
+  if (meals) {
+    meals.forEach((meal) => {
+      const mealItem = document.createElement('div');
+      mealItem.classList.add('meal-item');
+      mealItem.dataset.id = meal.idMeal;
+      mealItem.innerHTML = `
+              <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+              <h3>${meal.strMeal}</h3>
+          `;
+      mealList.appendChild(mealItem);
+    });
+  } else {
+    mealList.innerHTML = '<p>No meals found. Try another ingredient.</p>';
+  }
+}
